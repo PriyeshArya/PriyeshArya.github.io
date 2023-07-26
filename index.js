99a4b0df12333c2899eb12c27f56659991684a1f
Priyesh Arya <priyesh.arya6@gmail.com>
2023-07-26 19:15:24 +0530

    Fix Bug: Click Link in Mobile-Nav not closing Mobile-Menu on its own

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Get the mobile-nav element
   const mobileNav = document.querySelector(".mobile-nav");
 
-  // Get the mobile-menu button and cross-menu button
+  // Get the mobile-menu button, cross-menu button and mobile-nav links
   const mobileMenuButton = document.querySelector(".mobile-menu");
   const crossLogo = document.querySelector(".fa-xmark");
+  const mobileNavLinks = document.querySelectorAll(".mobile-nav a");
 
   function toggleMenu() {
     if (mobileNav.classList.contains("closed-menu")) {
@@ -16,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
-  // Add a click event listener to the mobile-menu button and cross-menu button
+  // Add a click event listener to the mobile-menu button, cross-menu button and each link in mobile-nav
   mobileMenuButton.addEventListener("click", toggleMenu);
   crossLogo.addEventListener("click", toggleMenu);
+  mobileNavLinks.forEach((link) => {
+    link.addEventListener("click", toggleMenu);
+  });
 });
